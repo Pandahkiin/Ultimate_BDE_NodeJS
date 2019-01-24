fb3e55b251b8c7d72162bf63462ebb46faf49792
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-24 11:59:41 +0100

    Correction

--- crud/app/routes/verifyJwtToken.js
+++ crud/app/routes/verifyJwtToken.js
@@ -8,17 +8,18 @@ var sql = require('../model/db.users_data');
 var verifyToken = function (req, res, next) {
   let token = req.headers['x-access-token'];
 
-  if(!token) {
-    return res.status(403).send({ auth : false, message : "No token provided (< x-access-token > header empty)" });
+  if(!!token) {
+    res.status(403).send({ auth : false, message : "No token provided (< x-access-token > header empty)" });
+  } else {
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if(err) {
+        res.status(500).send({ auth : false, message : "Fail to Authentication. Error -> " + err });
+      } else {
+        req.userId = decoded.id;
+        next();
+      }
+    });
   }
-
-  jwt.verify(token, config.secret, function(err, decoded) {
-    if(err) {
-      return res.status(500).send({ auth : false, message : "Fail to Authentication. Error -> " + err });
-    }
-    req.userId = decoded.id;
-    next();
-  });
 }
 
 var isBdeMember = function(req, res, next) {
