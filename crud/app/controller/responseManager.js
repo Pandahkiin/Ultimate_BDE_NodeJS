2e1832e44aa5211fc162812a689970f45d9a4a0b
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-24 16:50:34 +0100

    Message for Ok response

--- crud/app/controller/responseManager.js
+++ crud/app/controller/responseManager.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function created(res, result) { res.status(201).send({ id : result, message : "Enregistrement correctement créé", status : "success" }); }
-function ok(res, result)      { res.status(200).send({ result, status : "success" }); }
+function ok(res)      { res.status(200).send({ message : "Requête effectuée", status : "success" }); }
 function badRequest(res, err) { res.status(400).send({ err, status : "warning" }); }
 function notFound(res)        { res.status(404).send({ code : "ER_ID_NOT_FOUND", message : "Cet ID n'existe pas", status : "warning" }); }
 
@@ -17,7 +17,7 @@ exports.getAll = function(res, err, result) {
   if(err) {
     res.status(500).send({ error : true, response : null, status : "warning" });
   } else {
-    ok(res, result);
+    ok(res);
   }
 }
 
@@ -35,6 +35,6 @@ exports.byId = function(res, err, result) {
   } else if(err) {
     badRequest(res, err);
   } else {
-    ok(res, result);
+    ok(res);
   }
 }
