2a9ea0e51b78dbf295d53adc30a8101b89dc4a5e
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-24 16:46:29 +0100

    Added access on DELETE

--- crud/server.js
+++ crud/server.js
@@ -11,8 +11,9 @@ console.log('API server started on: ' + ip + ':' + port);
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
 });
 
