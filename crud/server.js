899682e68a7e6e2d1eb44f5cd148949ae3a65de2
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-25 19:20:35 +0100

    Cors deleted

--- crud/server.js
+++ crud/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const ip = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
@@ -11,10 +10,9 @@ console.log('API server started on: ' + ip + ':' + port);
 
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
-app.options('*', cors());
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
 });
