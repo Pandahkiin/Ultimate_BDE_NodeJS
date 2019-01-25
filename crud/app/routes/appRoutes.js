470f2302b1e4dac7124432507709155988867961
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-25 14:58:24 +0100

    Security increased

--- crud/app/routes/appRoutes.js
+++ crud/app/routes/appRoutes.js
@@ -10,33 +10,33 @@ module.exports = function(app) {
 
   //campuses Routes
   app.route('/api/campuses')
-    .get(campusesController.list_all_campuses)
+    .get([authJwt.verifyToken], campusesController.list_all_campuses)
     .post([authJwt.verifyToken, authJwt.isBdeMember], campusesController.create_a_campus);
 
   app.route('/api/campuses/:campusId')
-    .get(campusesController.read_a_campus)
+    .get([authJwt.verifyToken], campusesController.read_a_campus)
     .put([authJwt.verifyToken, authJwt.isBdeMember], campusesController.update_a_campus)
     .delete([authJwt.verifyToken, authJwt.isBdeMember], campusesController.delete_a_campus);
 
 
   //events Routes
   app.route('/api/events')
-    .get(eventsController.list_all_events)
+    .get([authJwt.verifyToken], eventsController.list_all_events)
     .post([authJwt.verifyToken, authJwt.isBdeMember], eventsController.create_an_event);
 
   app.route('/api/events/:eventId')
-    .get(eventsController.read_an_event)
+    .get([authJwt.verifyToken], eventsController.read_an_event)
     .put([authJwt.verifyToken, authJwt.isBdeMember], eventsController.update_an_event)
     .delete([authJwt.verifyToken, authJwt.isBdeMember], eventsController.delete_an_event);
 
 
   //goodies Routes
   app.route('/api/goodies')
-    .get(goodiesController.list_all_goodies)
+    .get([authJwt.verifyToken], goodiesController.list_all_goodies)
     .post([authJwt.verifyToken, authJwt.isBdeMember], goodiesController.create_a_goody);
 
   app.route('/api/goodies/:goodyId')
-    .get(goodiesController.read_a_goody)
+    .get([authJwt.verifyToken], goodiesController.read_a_goody)
     .put([authJwt.verifyToken, authJwt.isBdeMember], goodiesController.update_a_goody)
     .delete([authJwt.verifyToken, authJwt.isBdeMember], goodiesController.delete_a_goody);
 
