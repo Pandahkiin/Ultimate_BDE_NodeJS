46ce4b7246776ef3f3dd1a92b1807c24c27ff18b
Tank-White <33095440+Tank-White@users.noreply.github.com>
2019-01-25 16:36:20 +0100

    It works in administration so it works everywhere

--- crud/server.js
+++ crud/server.js
@@ -11,7 +11,7 @@ console.log('API server started on: ' + ip + ':' + port);
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
